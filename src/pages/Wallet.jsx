a996e169eb9cc3827ce135d5a1b6d569a43e23ee
Saulo Michielin <saulomichielin@gmail.com>
2023-01-06 20:04:25 -0300

    Pelejando no requisito 1...

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    const {
-      email,
-      senha,
-    } = this.props
     return (
       <>
         <div>TrybeWallet</div>
-        <p>{ email }</p>
-        <p>{ senha }</p>
       </>
     )
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-    ...state.user
-  }
-}
-
-export default connect(mapStateToProps)(Wallet);
