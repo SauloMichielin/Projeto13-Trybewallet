a1172b089dae23e28202256ea187925739411159
Saulo Michielin <saulomichielin@gmail.com>
2023-01-07 17:55:04 -0300

    HitLinter

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,7 +5,6 @@ import wallet from './wallet';
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
-
 const rootReducer = combineReducers({
   user,
   wallet,
